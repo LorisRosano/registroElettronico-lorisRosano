a32885b8a9179fb056d173d780dbefc53df52e11
LorisRosano <l.rosano.2306@vallauri.edu>
2023-05-19 13:17:08 +0200

    modificata pagina login + iniziato cambia password

--- index.js
+++ index.js
@@ -4,21 +4,21 @@ window.onload = () => {
     }
     $(".btnLogin").on("mouseover", () => {sus(this)});
     $(".btnLogin").on("mouseout", () => {sus(this)});
-
-    $("#codFiscaleScuola").animate({
-        width: "12vw",
-        height:"3vh"
-    }, 1000)
 };
 function apriPaginaLoginStudente(){
-    window.location = "studente.php";
+    window.location = "studente.html";
 }
 function ritornaHome(){
     window.location = "index.php";
 }
-function animationInput(input){
-    input.animate({
-        width: "12vw",
-        height:"3vh"
-    }, 1000)
+function cambiaPassword(){
+    let divLogin = $(".login-container");
+    divLogin.animate({height: "toggle", opacity: "toggle"}, "slow", () => {
+        let aus = $("#divLogin").children().not(".nascosto");
+        let aus2 = $("#divLogin").children(".nascosto");
+        aus.toggleClass("nascosto");
+        aus2.toggleClass("nascosto");
+        console.log(aus);
+    });
+    divLogin.animate({height: "toggle", opacity: "toggle"}, "slow");
 }
